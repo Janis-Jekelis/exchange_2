077189040706dc3ed14f9dac9ed5c0186bda9984
Janis <janisjekelis@gmail.com>
2023-10-26 12:24:52 +0300

    ex

--- index.php
+++ index.php
@@ -5,4 +5,4 @@ use App\Application;
 
 require_once "vendor/autoload.php";
 $result = new Application();
-echo number_format($result->calculate(), 2) . " " . $result->getTargetCurrency() . PHP_EOL;
+echo number_format($result->calculate(), 2) . " " . $result->getTargetCurrency() . PHP_EOL ;
